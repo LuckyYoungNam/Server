edca5684ae274fab3735c7a4fccbc3b7ed910350
@_.mung <wjdahrrla@gmail.com>
2024-10-28 10:57:31 +0900

    Chore: 프로젝트 세팅해라

---
+++ src/test/java/org/example/youngnam/YoungNamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.youngnam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungNamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
