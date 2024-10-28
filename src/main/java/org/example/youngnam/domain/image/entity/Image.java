ff925c247e25238e6b824b8eaa113a0fcae04518
@_.mung <wjdahrrla@gmail.com>
2024-10-28 22:58:19 +0900

    Feat: Image 테이블 세팅해라 - #1
    Image 테이블을 세팅함.
    
    구현한 내용:
    - Image.java 세팅
    - ImageStatus.java 세팅

--- src/main/java/org/example/youngnam/domain/image/entity/Image.java
+++ src/main/java/org/example/youngnam/domain/image/entity/Image.java
@@ -1,4 +1,44 @@
 package org.example.youngnam.domain.image.entity;
 
-public class Image {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.youngnam.global.base.BaseTimeEntity;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long imageId;
+    private String imageFormat;
+    private long userId;
+    @Enumerated(EnumType.STRING)
+    private ImageStatus imageStatus;
+
+    /** 원본 **/
+    private String imagePreUrl;
+    private String imagePrePath;
+    private String imagePreFileName;
+    private double imagePreWidth;
+    private double imagePreHeight;
+
+    /** 결과 **/
+    private String imageResultUrl;
+    private String imageResultPath;
+    private String imageResultFileName;
+    private double imageResultWidth;
+    private double imageResultHeight;
+
+    /** thumbnail **/
+    private String thumbnailUrl;
+    private String thumbnailPath;
+    private String thumbnailPathFileName;
+    private double thumbnailWidth;
+    private double thumbnailHeight;
 }
