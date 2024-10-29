b5ec4e9c50dd03d613c3b7fc35559d9b52c5d424
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-10-30 01:20:00 +0900

    Feat: security filter 구현 - #15

---
+++ src/main/java/org/example/youngnam/auth/config/SecurityConfig.java
@@ -0,0 +1,87 @@
+package org.example.youngnam.auth.config;
+
+import lombok.RequiredArgsConstructor;
+import org.example.youngnam.auth.exception.ExceptionHandlerFilter;
+import org.example.youngnam.auth.exception.JwtAuthenticationEntryPoint;
+import org.example.youngnam.auth.filter.JwtAuthenticationFilter;
+import org.example.youngnam.auth.jwt.JwtProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtProvider jwtProvider;
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
+    private static final String[] whiteList = {
+            "/actuator/health",
+            "/api/v1/users/check",
+            "/api/v1/users/signup",
+            "/api/v1/users/signin",
+            "/api/v1/users/reissue",
+            "/",
+            "/swagger-ui/**",
+            "/error",
+            "/v3/api-docs/**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors-> cors.configurationSource(corsConfigurationSource()))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagementConfigurer ->
+                        sessionManagementConfigurer
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(
+                        exceptionHandlingConfigurer -> exceptionHandlingConfigurer.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .authorizeHttpRequests(
+                        authorizationManagerRequestMatcherRegistry ->
+                                authorizationManagerRequestMatcherRegistry
+                                        .anyRequest()
+                                        .authenticated())
+
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(exceptionHandlerFilter, JwtAuthenticationFilter.class)
+                .build();
+    }
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", getDefaultCorsConfiguration());
+
+        return source;
+    }
+
+    private CorsConfiguration getDefaultCorsConfiguration() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        return configuration;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(whiteList);
+    }
+}
---
+++ src/main/java/org/example/youngnam/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,46 @@
+package org.example.youngnam.auth.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.youngnam.auth.jwt.JwtProvider;
+import org.example.youngnam.global.Constants;
+import org.example.youngnam.global.exception.ErrorCode;
+import org.example.youngnam.global.exception.UnauthorizedException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static org.example.youngnam.auth.filter.TokenAuthentication.createTokenAuthentication;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String accessToken = getAccessToken(request);
+        final long userId = jwtProvider.getUserIdFromSubject(accessToken);
+        doAuthentication(accessToken, userId);
+        filterChain.doFilter(request, response);
+    }
+
+    private String getAccessToken(final HttpServletRequest request) {
+        final String accessToken = request.getHeader(Constants.AUTHORIZATION);
+        if (StringUtils.hasText(accessToken) && accessToken.startsWith(Constants.BEARER)) {
+            return accessToken.substring(Constants.BEARER.length());
+        }
+        throw new UnauthorizedException(ErrorCode.UNAUTHORIZED);
+    }
+
+    private void doAuthentication(final String token, final long userId) {
+        TokenAuthentication tokenAuthentication = createTokenAuthentication(token, userId);
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(tokenAuthentication);
+    }
+}
