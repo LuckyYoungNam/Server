04f68e213f079a7e3a9ecd6452af2d257aa38a07
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-10-30 01:20:38 +0900

    Feat: argument resolver 구현 - #15

---
+++ src/main/java/org/example/youngnam/global/config/WebConfig.java
@@ -0,0 +1,22 @@
+package org.example.youngnam.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.example.youngnam.auth.resolver.UserIdArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final UserIdArgumentResolver userIdArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userIdArgumentResolver);
+    }
+}
