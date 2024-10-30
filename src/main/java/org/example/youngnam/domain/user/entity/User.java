5a1e9c7454cc141912f2aa5901582b9d32fe83b3
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-10-30 16:43:25 +0900

    Feat: 카카오 토큰정보 feign api 구현 - #17

--- src/main/java/org/example/youngnam/domain/user/entity/User.java
+++ src/main/java/org/example/youngnam/domain/user/entity/User.java
@@ -1,5 +1,6 @@
 package org.example.youngnam.domain.user.entity;
 
+import io.micrometer.core.annotation.Counted;
 import jakarta.persistence.*;
 import lombok.*;
 import org.example.youngnam.global.base.BaseTimeEntity;
@@ -16,7 +17,19 @@ import java.time.LocalDateTime;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
+
+    @Column(name = "social_id")
     private String socialId;
+
+    @Column(name = "naver_id")
+    private String naverId; //네이버 블로그용
+
+    public static User create(final String socialId) {
+        return User.builder()
+                .socialId(socialId)
+                .build();
+    }
 
 }
--- src/main/java/org/example/youngnam/domain/user/entity/UserStatus.java
+++
@@ -1,5 +0,0 @@
-package org.example.youngnam.domain.user.entity;
-
-public enum UserStatus {
-    ACTIVE, DIS_ACTIVE
-}
