d34f69ac92bbc237acfa0f9ea9cd7706436bfe88
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-11-01 22:42:28 +0900

    cors

--- src/main/java/org/example/youngnam/auth/config/CorsFilter.java
+++ src/main/java/org/example/youngnam/auth/config/CorsFilter.java
@@ -26,7 +26,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, ,X-Requested-With, Authorization, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.setHeader("Access-Control-Allow-Headers", "withCredentials, Access-Control-Allow-Headers, Origin, ,X-Requested-With, Authorization, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
 
         if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
