e7313df665c83494f5bb6914f8b0c7eb80f69e98
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-11-01 22:29:24 +0900

    cors

--- src/main/java/org/example/youngnam/auth/config/CorsFilter.java
+++ src/main/java/org/example/youngnam/auth/config/CorsFilter.java
@@ -24,7 +24,7 @@ public class CorsFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", "https://inuluckyyoungnam.vercel.app");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
